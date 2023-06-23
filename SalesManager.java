c925973652b83671455add033a0ec88452101754
Ikar-x <ikar86x@gmail.com>
2023-06-23 23:11:53 +0500

    stat

--- SalesManager.java
+++ SalesManager.java
@@ -14,4 +14,26 @@ public class SalesManager {
         }
         return max;
     }
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale > min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int mean() {
+        int min = min();
+        int max = max();
+        int sum = 0;
+        for (int sale : sales) {
+            if (sale == min) sale = 0;
+            if (sale == max) sale = 0;
+            sum += sale;
+        }
+        return Math.round(sum / (sales.length - 2));
+    }
+
 }
